407302930736eba0d2fda2cee8eee2823715b990
Pavel Zabalotny <pavelzabalotny@gmail.com>
2022-09-27 20:46:37 +0300

    feat: implement header filter layout

--- youtube-client/src/app/header/filter/filter.component.ts
+++ youtube-client/src/app/header/filter/filter.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
+  value = ''
 
   constructor() { }
 
