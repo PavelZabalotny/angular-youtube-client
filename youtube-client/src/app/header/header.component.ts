b97fabccbb418c033976af4e4f4159f405eca81a
Pavel Zabalotny <pavelzabalotny@gmail.com>
2022-09-28 22:35:00 +0300

    feat: implement settings button click logic

--- youtube-client/src/app/header/header.component.ts
+++ youtube-client/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Input() showFilter = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleShowFilter() {
+    this.showFilter = !this.showFilter
+  }
 }
--- youtube-client/src/app/header/search-input/search-input.component.ts
+++ youtube-client/src/app/header/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-input',
@@ -6,12 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-input.component.scss']
 })
 export class SearchInputComponent implements OnInit {
+  @Output() onClickSettings = new EventEmitter<boolean>()
   value = '';
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  handleClickSettings(): void {
+    this.onClickSettings.emit()
   }
 
 }
